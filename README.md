62088c628e81cfe7e23160fd6afa7e65a554fcb7
meebox <meebox@gmail.com>
2021-08-18 14:13:55 +0800

    Add diagonal scrolling method.

--- README.md
+++ README.md
@@ -5,16 +5,20 @@ The scroll method in the MicroPython's ssd1306 class is based on [FrameBuffer cl
 By adopting the [modified version of the ssd1306 class](https://github.com/timotet/SSD1306/blob/master/ssd1306.py) written by Tim Toliver, we create a new SSD1306_I2C_FLAG class to reuse the code already in the built-in ssd1306 class. The SSD1306_I2C_FLAG class has the following methods added:
 
 - clear()：clear the OLED.
-- hw_scroll_h(direction=True, frames=FRAMES_5):continuously scroll to right by default. The frames can be:
-    - SSD1306_FLAG_I2C.FRAMES_2
-    - SSD1306_FLAG_I2C.FRAMES_3
-    - SSD1306_FLAG_I2C.FRAMES_4
-    - SSD1306_FLAG_I2C.FRAMES_5
-    - SSD1306_FLAG_I2C.FRAMES_25
-    - SSD1306_FLAG_I2C.FRAMES_64
-    - SSD1306_FLAG_I2C.FRAMES_128
-    - SSD1306_FLAG_I2C.FRAMES_256
-    One frame approximate 9.19ms. 
+- hw_scroll_h(direction=True, interval=FRAMES_5):continuously scroll to right/left.
+    - direction: True/False, scroll to right/left.
+    - interval:
+        - SSD1306_FLAG_I2C.FRAMES_2
+        - SSD1306_FLAG_I2C.FRAMES_3
+        - SSD1306_FLAG_I2C.FRAMES_4
+        - SSD1306_FLAG_I2C.FRAMES_5
+        - SSD1306_FLAG_I2C.FRAMES_25
+        - SSD1306_FLAG_I2C.FRAMES_64
+        - SSD1306_FLAG_I2C.FRAMES_128
+        - SSD1306_FLAG_I2C.FRAMES_256
+        One frame approximate 9.19ms. 
+- hw_scroll_diag(direction=True, interval=FRAMES_5, offset=1):continuously scroll to up-right/up-left.
+    - offset: vertical offset per scroll.
 - hw_scroll_off()：stop scrolling.
 
 ## Usage example
@@ -32,11 +36,15 @@ while True:
     oled.text("I love PYTHON!", 0, 16)
     oled.text("I love PYTHON!", 0, 32)
     oled.show()
-    oled.hw_scroll_h(frames=oled.FRAMES_128)
+    oled.hw_scroll_h(interval=oled.FRAMES_128)
     sleep(2)
     oled.hw_scroll_h(False)
     sleep(2)
     oled.hw_scroll_off()
+    sleep(2)
+    oled.hw_scroll_diag(interval=oled.FRAMES_25)
+    sleep(2)
+    oled.hw_scroll_diag(False, offset=3)
     sleep(2)
     oled.clear()
     sleep(2)
--- ssd1306_i2c_flag.py
+++ ssd1306_i2c_flag.py
@@ -4,6 +4,8 @@ SET_HWSCROLL_OFF    = const(0x2e)
 SET_HWSCROLL_ON     = const(0x2f)
 SET_HWSCROLL_RIGHT  = const(0x26)
 SET_HWSCROLL_LEFT   = const(0x27)
+SET_HWSCROLL_VR     = const(0x29)
+SET_HWSCROLL_VL     = const(0x2a)
 
 class SSD1306_I2C_FLAG(SSD1306_I2C):
     FRAMES_2 = 0x07
@@ -24,7 +26,11 @@ class SSD1306_I2C_FLAG(SSD1306_I2C):
     def hw_scroll_off(self):
         self.write_cmd(SET_HWSCROLL_OFF) # turn off scroll
         
-    def hw_scroll_h(self, direction=True, frames=FRAMES_5):   # default to scroll right
+    def hw_scroll_h(
+        self, 
+        direction=True,    # True/False: right/left
+        interval=FRAMES_5  # one frame ≓ 9.2ms
+    ):
         self.write_cmd(SET_HWSCROLL_OFF)  # turn off hardware scroll per SSD1306 datasheet
         if not direction:
             self.write_cmd(SET_HWSCROLL_LEFT)
@@ -32,8 +38,27 @@ class SSD1306_I2C_FLAG(SSD1306_I2C):
             self.write_cmd(SET_HWSCROLL_RIGHT)
         self.write_cmd(0x00)            # dummy byte
         self.write_cmd(0x00)            # start page = page 0
-        self.write_cmd(frames)          # interval
+        self.write_cmd(interval)        # interval
         self.write_cmd(0x07)            # end page = page 7
         self.write_cmd(0x00)            # dummy byte
         self.write_cmd(0xff)            # dummy byte
+        self.write_cmd(SET_HWSCROLL_ON) # activate scroll
+
+    def hw_scroll_diag(
+        self, 
+        direction=True,    # True/False: right/left
+        interval=FRAMES_5, # one frame ≓ 9.2ms
+        offset=1           # vertical offset per scroll
+    ):
+
+        self.write_cmd(SET_HWSCROLL_OFF)  # turn off hardware scroll per SSD1306 datasheet
+        if not direction:
+            self.write_cmd(SET_HWSCROLL_VL)
+        else:
+            self.write_cmd(SET_HWSCROLL_VR)
+        self.write_cmd(0x00)            # dummy byte
+        self.write_cmd(0x00)            # start page = page 0
+        self.write_cmd(interval)        # interval
+        self.write_cmd(0x07)            # end page = page 7
+        self.write_cmd(offset)          # vertical offset per scroll
         self.write_cmd(SET_HWSCROLL_ON) # activate scroll
